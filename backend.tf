7f33140ec3d0889ddaa611bab7b258e6efb1c1a6
arou <bananaumai96@gmail.com>
2026-01-29 18:39:00 +0900

    webserver-infra

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "webserver-tfstate-001"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+  }
+}
