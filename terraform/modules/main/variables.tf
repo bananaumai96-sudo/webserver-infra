23127d15cd35d382067342296566b9bfb82724e1
arou <bananaumai96@gmail.com>
2026-01-31 22:23:48 +0900

    Update Terraform configuration

--- terraform/modules/main/variables.tf
+++ terraform/modules/main/variables.tf
@@ -12,3 +12,12 @@ variable "tag" {
   description = "tag"
   type        = map(string)
 }
+
+variable "ingress" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
